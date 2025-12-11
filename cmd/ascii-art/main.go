3de5e55c3da43626543f222ecb102c8b18a99ab1
sgkraika.com <stavros1989gkraikas@gmail.com>
2025-12-11 21:22:54 +0200

    Update for output->txt

--- cmd/ascii-art/main.go
+++ cmd/ascii-art/main.go
@@ -9,33 +9,40 @@ import (
 )
 
 func main() {
-	var colorFlag, substring, text string
+	var colorFlag, substring, text, outputFile string
 
 	// Parse arguments
-	switch len(os.Args) {
+	args := os.Args[1:]
+	for i, arg := range args {
+		if strings.HasPrefix(arg, "--output=") {
+			outputFile = strings.TrimPrefix(arg, "--output=")
+			args = append(args[:i], args[i+1:]...)
+			break
+		}
+	}
+
+	switch len(args) {
+	case 1:
+		text = args[0]
 	case 2:
-		text = os.Args[1]
-	case 3:
-		// --color=<color> "text"
-		if strings.HasPrefix(os.Args[1], "--color=") {
-			colorFlag = strings.TrimPrefix(os.Args[1], "--color=")
-			text = os.Args[2]
+		if strings.HasPrefix(args[0], "--color=") {
+			colorFlag = strings.TrimPrefix(args[0], "--color=")
+			text = args[1]
 		} else {
 			printUsage()
 			return
 		}
-	case 4:
-		// --color=<color> <substring> "text"
-		if strings.HasPrefix(os.Args[1], "--color=") {
-			colorFlag = strings.TrimPrefix(os.Args[1], "--color=")
-			substring = os.Args[2]
-			text = os.Args[3]
+	case 3:
+		if strings.HasPrefix(args[0], "--color=") {
+			colorFlag = strings.TrimPrefix(args[0], "--color=")
+			substring = args[1]
+			text = args[2]
 		} else {
 			printUsage()
 			return
 		}
 	default:
-		if len(os.Args) > 2 {
+		if len(args) > 0 {
 			printUsage()
 		}
 		return
@@ -61,7 +68,16 @@ func main() {
 			lines = ascii.ApplyColor(lines, substring, colorFlag, text, charMap)
 			result = strings.Join(lines, "\n")
 		}
-		fmt.Print(result + "\n")
+		
+		// Save to file or print to stdout
+		if outputFile != "" {
+			if err := ascii.SaveToFile(outputFile, result+"\n"); err != nil {
+				fmt.Fprintf(os.Stderr, "Error saving to file: %v\n", err)
+				os.Exit(1)
+			}
+		} else {
+			fmt.Print(result + "\n")
+		}
 	}
 }
 
