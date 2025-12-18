48797855c5db2a85f7511d4b7b5694e3290a6662
sgkraika.com <stavros1989gkraikas@gmail.com>
2025-12-18 22:39:10 +0200

    Update main for multiple banner usage

--- cmd/ascii-art/main.go
+++ cmd/ascii-art/main.go
@@ -9,7 +9,8 @@ import (
 )
 
 func main() {
-	var colorFlag, substring, text, outputFile string
+	var colorFlag, substring, text, outputFile, banner string
+	banner = "standard" // default banner
 
 	// Parse arguments
 	args := os.Args[1:]
@@ -29,14 +30,29 @@ func main() {
 			colorFlag = strings.TrimPrefix(args[0], "--color=")
 			text = args[1]
 		} else {
-			printUsage()
-			return
+			text = args[0]
+			banner = args[1]
 		}
 	case 3:
 		if strings.HasPrefix(args[0], "--color=") {
 			colorFlag = strings.TrimPrefix(args[0], "--color=")
+			if len(args) == 3 {
+				substring = args[1]
+				text = args[2]
+			} else {
+				text = args[1]
+				banner = args[2]
+			}
+		} else {
+			text = args[0]
+			banner = args[1]
+		}
+	case 4:
+		if strings.HasPrefix(args[0], "--color=") {
+			colorFlag = strings.TrimPrefix(args[0], "--color=")
 			substring = args[1]
 			text = args[2]
+			banner = args[3]
 		} else {
 			printUsage()
 			return
@@ -52,8 +68,9 @@ func main() {
 		return
 	}
 
-	// Load the standard banner
-	charMap, err := ascii.LoadBanner("assets/standard.txt")
+	// Load the specified banner
+	bannerFile := "assets/" + banner + ".txt"
+	charMap, err := ascii.LoadBanner(bannerFile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error loading banner: %v\n", err)
 		os.Exit(1)
